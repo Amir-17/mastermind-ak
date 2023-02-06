c51a64a95cea5915e0412b974e3bca889d78eb01
Amir-17 <amirkahriman17@gmail.com>
2023-02-06 14:49:08 +0100

    fixing some css and issues

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import Rules from "./Rules";
 import Board from "./Board";
 import Colors from "./Colors";
